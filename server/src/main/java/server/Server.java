2569dda4c02b98e39c51d3bacce1e7d67e01c463
Alex Kemp <akemp121@byu.edu>
2025-10-14 17:37:46 -0600

    Added bare-bones code to the endpoints so that there's a structure

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,6 +11,28 @@ public class Server {
 
         // Register your endpoints and exception handlers here.
 
+        // clear
+        javalin.delete("/db", ctx -> ctx.result("Clear not implemented"));
+
+        // register
+        javalin.post("/user", ctx -> ctx.result("Register not implemented"));
+
+        // login
+        javalin.post("/session", ctx -> ctx.result("Login not implemented"));
+
+        // logout
+        javalin.delete("/session", ctx -> ctx.result("Logout not implemented"));
+
+        // list games
+        javalin.get("/game", ctx -> ctx.result("List games not implemented"));
+
+        // create game
+        javalin.post("/game", ctx -> ctx.result("Create game not implemented"));
+
+        // join game
+        javalin.put("/game", ctx -> ctx.result("Join game not implemented"));
+
+
     }
 
     public int run(int desiredPort) {
